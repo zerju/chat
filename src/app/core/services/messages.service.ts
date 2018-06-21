8a6ad3fca79a215206958891253219f623621816
zerju <jure.zerak@gmail.com>
2018-06-21 15:53:53 +0200

    authenticate socket

--- src/app/core/services/messages.service.ts
+++ src/app/core/services/messages.service.ts
@@ -1,15 +1,20 @@
 import {Injectable} from '@angular/core';
 import * as io from 'socket.io-client';
 import {ISendMessage} from '../models/send-message.model';
+import {Store} from '@ngxs/store';
 
 @Injectable({providedIn: 'root'})
 export class MessagesService {
   private _socket: any;
-  constructor() {}
+  constructor(private _store: Store) {}
   connectSocket() {
+    const token = this._store.selectSnapshot<string>(
+        (state: any) => state.auth.accessToken);
     if (this._socket === undefined) {
-      this._socket = io('http://localhost:3000',
-                        {transports: ['websocket', 'polling', 'flashsocket']});
+      this._socket = io('http://localhost:3000', {
+        transports: ['websocket', 'polling', 'flashsocket'],
+        query: {token: token}
+      });
     }
   }
   sendMessage(message: ISendMessage) { this._socket.emit('message', message); }
